3f0ffa2ae07d5ee9374131bb5cdc30efde989f2b
Marc Fabre <nds.kram@gmail.com>
2018-05-30 18:14:39 +0200

    Display text

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -1,21 +1,32 @@
 <template>
-  <div v-wwo>
-    I AM WWOBJECT : {{wwObject.content.data.text}}
-  </div>
+	<div v-html="text">
+	</div>
 </template>
 
 
 <script>
+
 export default {
-  name: "ww-text",
-  props: {
-    wwObject: Object
-  },
-  data() {
-    return {};
-  },
-  methods: {},
-  computed: {},
-  mounted: function() {}
+	name: "ww-text",
+	props: {
+		wwObject: Object
+	},
+	data() {
+		return {
+			text: ""
+		};
+	},
+	methods: {
+	},
+	computed: {
+	},
+	mounted: function () {
+		this.text = wwLib.wwLang.getText(this.wwObject.content.data.text);
+
+		const self = this;
+		wwLib.$on('wwLang:changed', function (l) {
+			self.text = wwLib.wwLang.getText(self.wwObject.content.data.text);
+		});
+	}
 };
 </script>
