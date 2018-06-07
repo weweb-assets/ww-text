08606b08139f167999c7200315619dda787c8432
Kramerbaf <nds.kram@gmail.com>
2018-06-07 14:24:30 +0200

    Updated vue-cli

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -7,28 +7,55 @@
 
 
 <script>
-
 export default {
-	name: "ww-text",
-	props: {
-		wwObject: Object
-	},
-	data() {
-		return {
-			text: ""
-		};
-	},
-	methods: {
-	},
-	computed: {
-	},
-	mounted: function () {
-		this.text = wwLib.wwLang.getText(this.wwObject.content.data.text);
+  name: "ww-text",
+  props: {
+    wwObject: Object
+  },
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {},
+  computed: {},
+  created: function() {
+    const wwObjectName = "ww-text";
+    const wwEnableMetrics = false;
 
-		const self = this;
-		wwLib.$on('wwLang:changed', function (l) {
-			self.text = wwLib.wwLang.getText(self.wwObject.content.data.text);
-		});
-	}
+    wwLib.wwObject.registerContentType(
+      wwObjectName,
+      {
+        type: wwObjectName,
+        data: {
+          text: {
+            fr_FR: "Nouveau texte"
+          },
+          tag: "div", //h1, quote, code, ...
+          align: "",
+          font: "",
+          size: "",
+          color: "",
+          classes: [],
+          children: []
+        }
+      },
+      wwEnableMetrics
+    );
+  },
+  mounted: function() {
+    this.text = wwLib.wwLang.getText(this.wwObject.content.data.text);
+
+    wwLib.wwElementsStyle.applyAllStyles({
+      wwObject: this.wwObject,
+      lastWwObject: null,
+      element: this.$el
+    });
+
+    const self = this;
+    wwLib.$on("wwLang:changed", function(l) {
+      self.text = wwLib.wwLang.getText(self.wwObject.content.data.text);
+    });
+  }
 };
 </script>
