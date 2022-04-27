9a947ca212fe86405d9ca8220e4ecb29613f92e6
Aurélie VIOLETTE <orwel.violette@gmail.com>
2022-04-27 15:44:44 +0200

    Add text and tag

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,3 +1,24 @@
 <template>
-    <wwTextElement></wwTextElement>
+    <wwText :tag="tag" :text="text"></wwText>
 </template>
+
+<script>
+export default {
+    props: {
+        content: { type: Object, required: true },
+        wwElementState: { type: Object, required: true },
+    },
+    computed: {
+        tag() {
+            if (this.wwElementState.isInsideLink && this.content.tag === 'button') {
+                return 'div';
+            } else {
+                return this.content.tag;
+            }
+        },
+        text() {
+            return this.wwElementState.props.text;
+        },
+    },
+};
+</script>
--- ww-config.js
+++ ww-config.js
@@ -9,10 +9,7 @@ export default {
         infoTag: content => ({
             color: 'var(--ww-color-blue-500)',
             backgroundColor: 'var(--ww-color-blue-100)',
-            text:
-                content['_ww-text_tag'] === 'button'
-                    ? 'BTN'
-                    : content['_ww-text_tag'] && content['_ww-text_tag'].toUpperCase(),
+            text: content.tag === 'button' ? 'BTN' : content.tag && content.tag.toUpperCase(),
             action: () => {
                 wwLib.wwSearchBar.executeAction({
                     actions: wwLib.wwSearchBar.getEditSidebarActions('settings', 'custom-0'),
@@ -23,5 +20,26 @@ export default {
     options: {
         hyperlink: true,
     },
-    properties: {},
+    properties: {
+        tag: {
+            label: {
+                en: 'Html tag',
+                fr: 'Tag html',
+            },
+            section: 'settings',
+            type: 'TextSelect',
+            options: {
+                options: [
+                    { value: 'p', label: 'P' },
+                    { value: 'h1', label: 'H1' },
+                    { value: 'h2', label: 'H2' },
+                    { value: 'h3', label: 'H3' },
+                    { value: 'h4', label: 'H4' },
+                    { value: 'button', label: 'Button' },
+                    { value: 'div', label: 'Div' },
+                ],
+            },
+            defaultValue: 'p',
+        },
+    },
 };
